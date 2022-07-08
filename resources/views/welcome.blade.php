f87c0fc7c6f2ae7e52d9a1f153a52a9897125af2
JIshanRandika <jishanrandika@gmail.com>
2022-07-08 15:08:39 +0530

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
             @if (Route::has('login'))
                 <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a style="color: #e2e8f0" href="{{ url('/home') }}" class="welcomebutton text-sm text-gray-700 dark:text-gray-500">Home</a>
+                        <a style="color: #e2e8f0" href="{{ route('eligibleStudents.index') }}" class="welcomebutton text-sm text-gray-700 dark:text-gray-500">Home</a>
                     @else
                             <a style="color: #e2e8f0" href="{{ route('login') }}" class="welcomebutton text-sm text-gray-700 dark:text-gray-500">Log in</a>
 
