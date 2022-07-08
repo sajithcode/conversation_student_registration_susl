5a1aed8d7074fb00d9aca6c480d73a465d284705
JIshanRandika <jishanrandika@gmail.com>
2022-07-08 15:06:13 +0530

    eligible student table

---
+++ database/migrations/2022_07_08_022806_create_eligible_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEligibleStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eligible_students', function (Blueprint $table) {
+            $table->id();
+            $table->string('nameWithInitials');
+            $table->string('regNum');
+            $table->string('indexNum');
+            $table->string('faculty');
+            $table->string('department');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eligible_students');
+    }
+}
