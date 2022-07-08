35994ae4e8d3d0fac0958ba2d244592d4db83de4
JIshanRandika <jishanrandika@gmail.com>
2022-07-09 01:29:19 +0530

    update student registration table

--- database/migrations/2022_07_08_120133_create_student_registrations_table.php
+++ database/migrations/2022_07_08_120133_create_student_registrations_table.php
@@ -16,9 +16,26 @@ class CreateStudentRegistrationsTable extends Migration
         Schema::create('student_registrations', function (Blueprint $table) {
             $table->id();
 
-            $table->string('indexNum');
+            $table->string('nameWithInitial');
+            $table->string('fullNameInEnglishBlock');
+            $table->string('fullNameInSinhala');
+            $table->string('gender');
             $table->string('nic');
+            $table->string('address');
             $table->string('mobileNumber');
+            $table->string('email');
+            $table->string('degreeName');
+            $table->string('regNum');
+            $table->string('indexNum');
+            $table->string('monthExamination');
+            $table->string('yearExamination');
+            $table->string('degreeClass');
+            $table->string('attendance');
+            $table->string('nameVisitor1');
+            $table->string('nameVisitor2');
+            $table->string('nicVisitor1');
+            $table->string('nicVisitor2');
+            $table->string('signedDate');
 
             $table->timestamps();
         });
