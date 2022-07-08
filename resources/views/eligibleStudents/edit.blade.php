6899b7dc986bbc1677a830250a9829215ba4fee4
JIshanRandika <jishanrandika@gmail.com>
2022-07-09 01:31:37 +0530

    update eligible student edit

--- resources/views/eligibleStudents/edit.blade.php
+++ resources/views/eligibleStudents/edit.blade.php
@@ -79,7 +79,7 @@
                 </div>
 
                 <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
                 </div>
             </div>
 
