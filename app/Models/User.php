aca43b5069cad11a3d1e83c635133dd4b6af03b0
JIshanRandika <jishanrandika@gmail.com>
2022-07-09 11:51:05 +0530

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'index',
+        'regNum',
         'name',
         'email',
         'password',
