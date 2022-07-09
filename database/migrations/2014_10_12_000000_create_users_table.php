ea9eec893d6d3264ecf95b310ace3b019bf59a89
JIshanRandika <jishanrandika@gmail.com>
2022-07-09 11:47:27 +0530

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('index');
+            $table->string('regNum');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
