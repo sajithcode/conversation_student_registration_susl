faf6ab4e26a56e1b862aeba15e2366caa85c137f
JIshanRandika <jishanrandika@gmail.com>
2022-07-10 00:33:12 +0530

    select registered students

--- resources/views/eligibleStudents/index.blade.php
+++ resources/views/eligibleStudents/index.blade.php
@@ -110,8 +110,57 @@
                 </div>
             </div>
 
+{{--            <div class="row" style="margin-bottom: 10px">--}}
+{{--                <div class="col-xs-12 col-sm-12 col-md-12">--}}
+{{--                    <div class="pull-right">--}}
+{{--                        <a class="btn btn-info" href="{{ route('eligibleStudents.show') }}">Registered student List</a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+
         @endif
-        <table class="table table-bordered">
+
+
+            <script>
+
+                $(function() {
+
+                    // run on change for the selectbox
+                    $( "#frm_duration" ).change(function() {
+                        updateDurationDivs();
+                    });
+
+                    // handle the updating of the duration divs
+                    function updateDurationDivs() {
+                        // hide all form-duration-divs
+                        $('.form-duration-div').hide();
+
+                        var divKey = $( "#frm_duration option:selected" ).val();
+                        $('#divFrm'+divKey).show();
+                    }
+
+                    // run at load, for the currently selected div to show up
+                    updateDurationDivs();
+
+                });
+            </script>
+
+            @if(checkPermission(['examinationBranch','mainStoreClark','viceChancellor']))
+{{--            <div class="col-xs-12 col-sm-12 col-md-12">--}}
+                <div class="form-group">
+                    <div>
+                        <select required name="attendance" class="form-control" id="frm_duration">
+{{--                            <option selected>Choose</option>--}}
+                            <option selected value="All">All Eligible Students</option>
+                            <option value="Registered">Registered Students</option>
+                        </select>
+                    </div>
+                </div>
+{{--            </div>--}}
+            @endif
+
+
+        <table id="divFrmAll" style="display:none" class="table table-bordered form-duration-div">
             <tr>
                             <th>No</th>
                 <th>Name</th>
@@ -163,6 +212,71 @@
                 </tr>
             @endforeach
         </table>
+
+
+{{--            ===========Registered Table==========--}}
+
+            <table id="divFrmRegistered" style="display:none" class="table table-bordered form-duration-div">
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Register Number</th>
+                    <th>Index Number</th>
+                    <th>Faculty</th>
+                    <th>Department</th>
+                    @if(checkPermission(['examinationBranch','mainStoreClark','viceChancellor']))
+                        <th width="280px">Action</th>
+                    @endif
+                </tr>
+                @php
+                    $a = 0
+                @endphp
+                @foreach ($eligibleStudents as $eligibleStudent)
+                    <tr>
+                        @foreach ($studentRegistrations as $studentRegistration)
+                            @if (strtoupper(trim($studentRegistration->regNum)) === strtoupper(trim($eligibleStudent->regNum)))
+
+                            {{--                        ==--}}
+                        <td>{{ ++$a }}</td>
+                        <td>{{ $eligibleStudent->nameWithInitials }}</td>
+                        <td>{{ $eligibleStudent->regNum }}</td>
+                        <td>{{ $eligibleStudent->indexNum }}</td>
+                        <td>{{ $eligibleStudent->faculty }}</td>
+                        <td>{{ $eligibleStudent->department }}</td>
+                        @if(checkPermission(['examinationBranch','mainStoreClark','viceChancellor']))
+                            <td>
+                                <form action="{{ route('eligibleStudents.destroy',$eligibleStudent->id) }}" method="POST">
+
+
+
+
+
+                                    @if(checkPermission(['examinationBranch']))
+                                        <a class="btn btn-primary" href="{{ route('eligibleStudents.edit',$eligibleStudent->id) }}">Edit</a>
+
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    @endif
+
+                                                    <a class="btn btn-info" href="{{ route('studentRegistration.show',$studentRegistration->id) }}">Registered</a>
+
+
+                                </form>
+                            </td>
+                        @endif
+
+                        @endif
+                        @endforeach
+{{--                        ==--}}
+                    </tr>
+                @endforeach
+            </table>
+
+
+
+
     </div>
 
 
