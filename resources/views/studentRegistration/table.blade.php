47b56f71fd99fb613edf4b545a48f5f00f3704dc
JIshanRandika <jishanrandika@gmail.com>
2022-07-12 12:21:36 +0530

    update export table

--- resources/views/studentRegistration/table.blade.php
+++ resources/views/studentRegistration/table.blade.php
@@ -7,6 +7,24 @@
         <th>Full Name In English Block Letter</th>
         <th>Full Name In Sinhala</th>
         <th>Email</th>
+        <th>NIC</th>
+        <th>Faculty</th>
+        <th>Department</th>
+        <th>Degree Name</th>
+        <th>Degree Class</th>
+
+        <th>Gender</th>
+        <th>Mobile Number</th>
+        <th>Address</th>
+        <th>Month and year of examination</th>
+        <th>Attendance</th>
+        <th>Visitor 1 Name</th>
+        <th>Visitor 1 NIC</th>
+        <th>Visitor 2 Name</th>
+        <th>Visitor 2 NIC</th>
+        <th>Signed Date</th>
+        <th>Status</th>
+
     </tr>
     </thead>
     <tbody>
@@ -18,6 +36,24 @@
             <td>{{ $studentRegistration->fullNameInEnglishBlock }}</td>
             <td>{{ $studentRegistration->fullNameInSinhala }}</td>
             <td>{{ $studentRegistration->email }}</td>
+            <td>{{ $studentRegistration->nic }}</td>
+            <td>{{ $studentRegistration->faculty }}</td>
+            <td>{{ $studentRegistration->department }}</td>
+            <td>{{ $studentRegistration->degreeName }}</td>
+            <td>{{ $studentRegistration->degreeClass }}</td>
+
+            <td>{{ $studentRegistration->gender }}</td>
+            <td>{{ $studentRegistration->mobileNumber }}</td>
+            <td>{{ $studentRegistration->address }}</td>
+            <td>{{ $studentRegistration->monthAndYearExamination }}</td>
+            <td>{{ $studentRegistration->attendance }}</td>
+            <td>{{ $studentRegistration->nameVisitor1 }}</td>
+            <td>{{ $studentRegistration->nicVisitor1 }}</td>
+            <td>{{ $studentRegistration->nameVisitor2 }}</td>
+            <td>{{ $studentRegistration->nicVisitor2 }}</td>
+            <td>{{ $studentRegistration->signedDate }}</td>
+            <td>{{ $studentRegistration->status }}</td>
+
 
         </tr>
     @endforeach
